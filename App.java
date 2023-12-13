9c3edf904f55b617eb07896f4a80fa68fb740553
amol <amol@tradingview.com>
2023-12-13 19:20:22 +0200

    Initial commit

---
+++ App.java
@@ -0,0 +1,21 @@
+
+
+
+public class App {
+	static class Toy {
+		int id;
+		String title;
+		int lots;
+		double weight;
+		public Toy(int id, String title, int lots, double weight) {
+			id = id;
+			title = title;
+			lots = lots;
+			weight = weight;
+		}
+	}
+	public static void main(String[] args) {
+
+		return ;
+	}
+}
